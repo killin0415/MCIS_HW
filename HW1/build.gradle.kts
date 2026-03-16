0cc40b624f4fb283c6afb4bd21493e00b4613cc6
killin0415 <swordfire1000@gmail.com>
2026-03-16 13:58:03 +0800

    chore: resolve merge conflicts in build.gradle.kts

--- HW1/build.gradle.kts
+++ HW1/build.gradle.kts
@@ -1,3 +1,6 @@
+buildscript {
+    val agp_version by extra("9.1.0")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "9.1.0" apply false
